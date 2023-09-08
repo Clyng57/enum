7ceb73d5a14cb85026c77305012ba0a2971ae8f2
Clyng57 <clyng57@gmail.com>
2023-09-08 12:04:01 -0500

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,31 +1,52 @@
 
 declare module '@neumatter/enum'
 
-declare class RevivedEnum extends Enum {}
+declare class RevivedEnum<T extends string | number | symbol | undefined> extends Enum<T> {}
 
 type EnumJSON<T> = Omit<T, 'keys'>
 
-export default class Enum<T extends string | number | symbol> {
-  static from<T> (obj: T): typeof RevivedEnum & { [P in keyof T]: RevivedEnum<T[P]> }
-  static initialize<T> (obj: T): typeof this & { [P in keyof T]: Enum<T[P]> }
+export type OmitIndexSignature<ObjectType> = {
+	[KeyType in keyof ObjectType as {} extends Record<KeyType, unknown>
+		? never
+		: KeyType]: ObjectType[KeyType];
+}
 
-  static build (): typeof Enum<T>
+export type PickIndexSignature<ObjectType> = {
+	[KeyType in keyof ObjectType as {} extends Record<KeyType, unknown>
+		? KeyType
+		: never]: ObjectType[KeyType];
+}
 
-  static get (input: Enum<T>['key'] | Enum<T>['value']): Enum<T>
+type SimpleMerge<Destination, Source> = {
+	[Key in keyof Destination as Key extends keyof Source ? never : Key]: Destination[Key];
+} & Source
 
-  static getKey (input: Enum<T>['key'] | Enum<T>['value']): Enum<T>['key']
+export type Merge<Destination, Source> = SimpleMerge<PickIndexSignature<Destination>, PickIndexSignature<Source>>
+& SimpleMerge<OmitIndexSignature<Destination>, OmitIndexSignature<Source>>
 
-  static getValue (input: Enum<T>['key'] | Enum<T>['value']): Enum<T>['value']
+type ToString<Type extends string | number | undefined> = `${Type}`
 
-  static has (input: Enum<T>['key'] | Enum<T>['value']): boolean
+export default class Enum<T extends string | number | undefined> {
+  static from<T extends Record<string | number, string | number>> (obj: T): typeof Enum & { [P in keyof T]: Enum<T[P]> }
+  static initialize<T extends Record<string | number, string | number>> (obj: T): typeof Enum & { [P in keyof T]: Enum<T[P]> }
 
-  static values (): IterableIterator<Enum<T>['value']>
+  static build (): typeof Enum
 
-  static entries (): IterableIterator<Enum<T>>
+  static get<T extends string | number> (input: T): Enum<T>
 
-  static [Symbol.iterator] (): IterableIterator<Enum<T>>
+  static getKey<T extends string | number> (input: T): Enum<T>['key']
 
-  static toJSON (): EnumJSON<Enum<T>>
+  static getValue<T extends string | number> (input: T): Enum<T>['value']
+
+  static has<T extends string | number> (input: T | Enum<string | number>): boolean
+
+  static values<T extends string | number> (): IterableIterator<Enum<T>['value']>
+
+  static entries<T extends string | number> (): IterableIterator<Enum<T>>
+
+  static [Symbol.iterator]<T extends string | number> (): IterableIterator<Enum<T>>
+
+  static toJSON<T extends string | number> (): EnumJSON<Enum<T>>
 
   static inspect (): string
 
@@ -33,17 +54,13 @@ export default class Enum<T extends string | number | symbol> {
   value: T
   ordinal: number
 
-  /**
-   *
-   * @param {T} value
-   */
-  constructor (value?: T | undefined)
+  constructor (value?: T)
 
-  is (input: Enum<T>['key'] | Enum<T>['value'] | Enum<T>): boolean
+  is (input: this['key'] | this['value'] | this): boolean
 
   toKeyString (): `${Enum<T>['constructor']['name']}.${Enum<T>['key']}`
 
-  toString (): `${Enum<T>['value']}`
+  toString (): ToString<this['value']>
 
   toJSON (): Enum<T>['value']
 }
